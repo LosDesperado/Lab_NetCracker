b80fb0f58d81eaa69b9f2d92abf26d8496e8eef3
LosDesperado <turbomax.94@yandex.ru>
2018-11-21 08:41:38 +0300

    Client-server CRUD

---
+++ Hero_lab/build/generated-sources/ap-source-output/jpa/entities/Hero_.java
@@ -0,0 +1,19 @@
+package jpa.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-20T16:04:33")
+@StaticMetamodel(Hero.class)
+public class Hero_ { 
+
+    public static volatile SingularAttribute<Hero, Character> alive;
+    public static volatile SingularAttribute<Hero, Long> phone;
+    public static volatile SingularAttribute<Hero, String> universe;
+    public static volatile SingularAttribute<Hero, String> description;
+    public static volatile SingularAttribute<Hero, Long> id;
+    public static volatile SingularAttribute<Hero, Short> power;
+    public static volatile SingularAttribute<Hero, String> hname;
+
+}
---
+++ Hero_lab/src/java/jpa/entities/Hero.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Max
+ */
+@Entity
+@Table(name = "HERO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Hero.findAll", query = "SELECT h FROM Hero h")
+    , @NamedQuery(name = "Hero.findById", query = "SELECT h FROM Hero h WHERE h.id = :id")
+    , @NamedQuery(name = "Hero.findByHname", query = "SELECT h FROM Hero h WHERE h.hname = :hname")
+    , @NamedQuery(name = "Hero.findByUniverse", query = "SELECT h FROM Hero h WHERE h.universe = :universe")
+    , @NamedQuery(name = "Hero.findByPower", query = "SELECT h FROM Hero h WHERE h.power = :power")
+    , @NamedQuery(name = "Hero.findByDescription", query = "SELECT h FROM Hero h WHERE h.description = :description")
+    , @NamedQuery(name = "Hero.findByAlive", query = "SELECT h FROM Hero h WHERE h.alive = :alive")
+    , @NamedQuery(name = "Hero.findByPhone", query = "SELECT h FROM Hero h WHERE h.phone = :phone")})
+public class Hero implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "HNAME")
+    private String hname;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "UNIVERSE")
+    private String universe;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "POWER")
+    private short power;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1200)
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ALIVE")
+    private Character alive;
+    @Column(name = "PHONE")
+    private Long phone;
+
+    public Hero() {
+    }
+
+    public Hero(Long id) {
+        this.id = id;
+    }
+
+    public Hero(Long id, String hname, String universe, short power, String description, Character alive) {
+        this.id = id;
+        this.hname = hname;
+        this.universe = universe;
+        this.power = power;
+        this.description = description;
+        this.alive = alive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHname() {
+        return hname;
+    }
+
+    public void setHname(String hname) {
+        this.hname = hname;
+    }
+
+    public String getUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(String universe) {
+        this.universe = universe;
+    }
+
+    public short getPower() {
+        return power;
+    }
+
+    public void setPower(short power) {
+        this.power = power;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Character getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Character alive) {
+        this.alive = alive;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Hero)) {
+            return false;
+        }
+        Hero other = (Hero) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jpa.entities.Hero[ id=" + id + " ]";
+    }
+    
+}
